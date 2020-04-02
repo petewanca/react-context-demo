807973dbf7bcd14df53c6f935ef760c0c1c6cea9
Pete Wanca <pwanca@xsolis.com>
2020-04-02 09:12:24 -0500

    Updates

---
+++ precache-manifest.57702116f52d69fac8b669ca7bb47ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473c5d522fd9c76347d6bc98e6264950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92e6f9075b21585ff21d",
+    "url": "/static/css/main.fdc74728.chunk.css"
+  },
+  {
+    "revision": "b42897e997e9c642836e",
+    "url": "/static/js/2.f1304d7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f1304d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e6f9075b21585ff21d",
+    "url": "/static/js/main.7393a99a.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
